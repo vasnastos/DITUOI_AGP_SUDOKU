f5734acde2dcc3bbb0dd0b784ab311dd1e0853ad
NASTOS VASILEIOS <63415169+vasnastos@users.noreply.github.com>
2021-05-21 10:19:41 +0300

    Add files via upload

---
+++ readdatatemplate.py
@@ -0,0 +1,50 @@
+    
+import os
+import sys
+
+default=os.path.join('','sudoku.sod')
+
+def Formatter(data):
+        totallinelength = len(data[0])+4
+        print(str('.'+'-'*6+'')*3, end='')
+        print('.')
+        counter = 0
+        for x in data:
+            if counter % 3 == 0 and counter != 0:
+                print(':', end='')
+                print('------ '*2, end='')
+                print('------:')
+            print('|', end='')
+            for j in range(0, len(x), 3):
+                print(str(x[j]) if x[j] != '0' else '.', str(x[j+1]) if x[j+1] !=
+                      '0' else '.', str(x[j+2]) if x[j+2] != '0' else '.', '|', end='')
+            print()
+            counter += 1
+        print(str('.'+'-'*6+'')*3+'.')
+
+def StringRead():
+    sudokustart=''
+    if len(sys.argv)!=2:
+          with open(default,'r') as F:
+              sudokustart=F.readline().strip()
+    else:
+          sudokustart=sys.argv[1]
+    if len(sudokustart)!=81:
+          with open(default,'r') as F:
+              sudokustart=F.readline().strip()
+    data = list()
+        # 15 χαρακτήρες η κάθε γραμμή
+        # 13 γραμμές συνολικά
+    counter = 9
+    previouscounter = 0
+    while counter <= 81:
+            data.append(sudokustart[previouscounter:counter])
+            previouscounter = counter
+            counter += 9
+    return data
+
+def main():
+      sudokustart=StringRead()
+      Formatter(sudokustart) 
+
+main()
